60c1440f28f43e0f5d79554156815322417c6c2e
Stefan Talbot <satalbot@protonmail.com>
2023-01-08 16:13:00 -0500

    Add Validations, and Seeds for User Model
    + Added: Validations for user model.
    + Added: Seed for user model.
    + Added: Ran migrations.

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -12,3 +12,12 @@ Example.destroy_all
 ex1 = Example.create(name: 'Ruby', price_cents: 100000, is_rare: true)
 ex2 = Example.create(name: 'Topaz', price_cents: 800, is_rare: false)
 ex3 = Example.create(name: 'Sapphire', price_cents: 80000, is_rare: true)
+
+## Create users.
+puts "Creating users!"
+User.destroy_all
+user1 = User.create(first_name: 'John', last_name: 'Doe', email: 'john_doe@email.com', password: 'password', img_url: 'https://imgur.com/avMgDEG')
+user2 = User.create(first_name: 'Jane', last_name: 'Smith', email: 'jane_smith@email.com', password: 'password', img_url: 'https://imgur.com/AK54oyB')
+user3 = User.create(first_name: 'Kelvin', last_name: 'Huang', email: 'kelvin.huang98@hotmail.com', password: 'password', img_url: 'https://imgur.com/bVrBJ3i')
+user4 = User.create(first_name: 'Stefan', last_name: 'Talbot', email: 'satalbot@protonmail.com', password: 'password', img_url: 'https://imgur.com/vMY7lhj')
+user5 = User.create(first_name: 'Rahim', last_name: 'Jamal', email: 'rahimj2196@gmail.com', password: 'password', img_url: 'https://imgur.com/YNVkpBM')
