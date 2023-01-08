81da9bb8ee4a0af647aba77a1475493105a02f24
Stefan Talbot <satalbot@protonmail.com>
2023-01-08 17:12:45 -0500

    Change User Routes
    - Removed: All routes from user except create.

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  resources :users
   get 'examples/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   # Defines the root path route ("/")
   # root "articles#index"
-
+  
+  resources :users, only: [:create]
   resources :examples, only: [:index]
 end
