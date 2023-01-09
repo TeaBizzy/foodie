87a793f6d99d7c4f41c0599e6d6f3bb2e5193786
kelvinhuang98 <kelvin.huang98@hotmail.com>
2023-01-08 22:24:22 -0500

    Implement nav bar component and associated stylings

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,39 +1,10 @@
-import { useState } from 'react';
 import './App.css';
-import axios from 'axios';
-import Example from './components/example';
-
-// Example App to showcase fetching data from the rails backend and displaying it with React.
-// We can request data from "http://localhost3000/RESOURCE-PATH-HERE".
-
-const EXAMPLE_API_URL = 'http://localhost:3000/examples'
+import Navbar from './components/Navbar';
 
 function App() {
-  // Example State.
-  const [examples, setExamples] = useState([]);
-  
-  // Called by the button.
-  function getExampleData() {
-    axios.get(EXAMPLE_API_URL).then(res => setExamples(res.data))
-  }
-
-  // An array containing JSX components.
-  const exampleList = examples.map((example) => {
-    return (
-      <Example 
-        key={example.id}
-        name={example.name}
-        price_cents={example.price_cents}
-        isRare={example.is_rare}
-      />
-    )
-  })
-
   return (
     <div className="App">
-      <h1>💎 Types of Gems 💎</h1>
-      {examples.length < 1 && <button onClick={getExampleData}>Fetch Data</button>}
-      {exampleList}
+      <Navbar />
     </div>
   );
 }
