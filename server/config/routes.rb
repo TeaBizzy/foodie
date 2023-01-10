6c2b7e439ea650c92582bdeddcc5a0102e445a9f
Stefan Talbot <satalbot@protonmail.com>
2023-01-10 14:36:50 -0500

    Add Restaurant Controller and Routes
    + Added: Controller for Restaurants.
    + Added: index route for Restaurants.

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   get 'examples/index'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
   # Defines the root path route ("/")
   # root "articles#index"
-  
+  resources :restaurants, only: [:index]
   resources :users, only: [:create, :index]
   resources :examples, only: [:index]
 end
