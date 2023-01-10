2115ac5469479e5974564693363fe18f4e1b49a3
kelvinhuang98 <kelvin.huang98@hotmail.com>
2023-01-10 18:13:41 -0500

    Add participants syling

--- client/src/routes/Create.jsx
+++ client/src/routes/Create.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import DateDetails from '../components/DateDetails'
 import LocationDetails from '../components/LocationDetails'
+import ParticipantDetails from '../components/ParticipantDetails'
 
 const Create = () => {
   return (
@@ -11,6 +12,7 @@ const Create = () => {
       <div className="session-details">
         <DateDetails />
         <LocationDetails />
+        <ParticipantDetails />
       </div>
     </div>
   )
