4256b4e7b8000a1fbe6f7d5d735ea4719ad47ba1
Stefan Talbot <satalbot@protonmail.com>
2023-01-10 14:03:50 -0500

    Changed DB Schema
    - Removed: Hour model and migration.
    + Added: Operating_time model and migration.
    # Modified: Seeds to reflect new schema changes.
    # Modified Restaurant model to reflect new schema changes.

--- server/app/models/restaurant.rb
+++ server/app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ApplicationRecord
-  has_many :hours, dependent: :destroy
+  has_many :operating_times, dependent: :destroy
   has_many :pictures, dependent: :destroy
 
   # No need for validations, as data doesn't come from users.
