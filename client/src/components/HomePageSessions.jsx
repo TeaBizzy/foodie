b87e76ae33ea56a4709691c7ddeded8a2a796596
kelvinhuang98 <kelvin.huang98@hotmail.com>
2023-01-11 04:10:12 -0500

    Implement styling for status bar in session cards and add it into session cards

--- client/src/components/HomePageSessions.jsx
+++ client/src/components/HomePageSessions.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import keg from "../assets/keg.jpg"
+import New from "./Status/New"
 import "./HomePageSessions.css"
 
 const HomePageSessions = () => {
   return (
     <div className="sessions-container">
       <div className="session">
-        <img className="session-image" src={keg}></img>
         <div className="session-left">
+          <img className="session-image" src={keg}></img>
           <div className="restaurant-container">
             <div className="restaurant-details">
               <span className="restaurant-name">Restaurant Name</span>
@@ -16,6 +17,18 @@ const HomePageSessions = () => {
             <div className="session-details">
               <span className="session-date">Session Date</span>
             </div>
+          </div>
+        </div>
+        <div className="session-right">
+          <New />
+          <div className="session-participants">
+            <span className="session-user"></span>
+            <span className="session-user"></span>
+            <span className="session-user"></span>
+          </div>
+          <div className="session-buttons">
+            <button className="session-edit-button">Edit</button>
+            <button className="session-cancel-button">Cancel</button>
           </div>
         </div>
       </div>
