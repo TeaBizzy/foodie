5340a633849a95668534243c46a6bb7eb63bc01b
Stefan Talbot <satalbot@protonmail.com>
2023-01-12 11:50:26 -0500

    Fix Model Relations

--- server/app/models/user_session.rb
+++ server/app/models/user_session.rb
@@ -1,7 +1,7 @@
 class UserSession < ApplicationRecord
   belongs_to :user
   belongs_to :session
-
+  
   validates :status, presence: true, length: { minimum: 0, maximum: 75 }
 
   attribute :status, default: 0
