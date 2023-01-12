37504d39d6cb8035ff68fa0f480cd94c8a746173
Stefan Talbot <satalbot@protonmail.com>
2023-01-11 20:31:41 -0500

    Connect Logout Button
    + Added: Logout functionality to navbar.

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,15 +1,24 @@
+import axios from 'axios'
 import React from 'react'
 import { FaSignOutAlt } from 'react-icons/fa'
+import { useNavigate } from 'react-router-dom'
 
 import "./Navbar.css"
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  
+  function logout() {
+    axios('http://localhost:3000/logout', {withCredentials: true})
+      .then(navigate('/login'))
+  }
+
   return (
     <div className="header">
       <h1 className="navbar-h1">Foodie</h1>
       <div className='user'>
         <span className='user-icon'></span>
-        <FaSignOutAlt size={40} style={{color: '#EC1562'}}/>
+        <FaSignOutAlt size={40} style={{color: '#EC1562'}} onClick={() => logout()}/>
       </div>
     </div>
   )
