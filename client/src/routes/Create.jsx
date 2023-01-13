ff1e2aae79dbae60f1d256a9c949b96041868557
Stefan Talbot <satalbot@protonmail.com>
2023-01-13 13:42:59 -0500

    Refactor
    # Refactored: Removed unused import.

--- client/src/routes/Create.jsx
+++ client/src/routes/Create.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 
-import Navbar from '../components/Navbar'
 import DateDetails from '../components/DateDetails'
 import LocationDetails from '../components/LocationDetails'
 import ParticipantDetails from '../components/ParticipantDetails'
