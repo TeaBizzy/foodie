043e0ece1a82b0e981725061b9e0ee5b62d8b331
Stefan Talbot <satalbot@protonmail.com>
2023-01-13 16:03:28 -0500

    Refactor
    # Refactored: Separated logic into separate functions.
    # Refactored: Added comments.

--- server/app/controllers/restaurants_controller.rb
+++ server/app/controllers/restaurants_controller.rb
@@ -5,37 +5,49 @@ class RestaurantsController < ApplicationController
   end
 
   def create_swipe
-    puts 'Calling!'
-    @restaurant_id = params[:restaurant_id]
-    @is_approved = params[:is_approved]
-    @restaurant = Restaurant.find_by(id: @restaurant_id)
-    @swipe = @restaurant.swipes.new(is_approved: @is_approved)
+    restaurant_id = params[:restaurant_id]
+    is_approved = params[:is_approved]
+    restaurant = Restaurant.find_by(id: restaurant_id)
+    @session = restaurant.session
+    
+    # Create the swipe.
+    swipe = restaurant.swipes.create(is_approved: is_approved)
 
-    puts 'SAVING SWIPE'
-    if(!@swipe.save)
-      puts 'Something went wrong!'
-      return
+    if(is_session_finished)
+      set_session_to_finished
+    else
+      set_current_user_session_to_pending # Must run first
+    end
+  end
+
+  # Sets the logged in users session status to Pending.
+  def set_current_user_session_to_pending
+    logged_in_user = session[:current_user_id]
+    user_session = @session.user_sessions.find_by(user_id: logged_in_user)
+    
+    if (user_session.status == 0)
+      user_session.update(status: 1)
+    end
+  end
+
+  # Checks if all invited users have swipped
+  def is_session_finished
+    swipe_count = 0
+    @session.restaurants.each do |restaurant|
+      swipe_count += restaurant.swipes.length
     end
 
-    # Change the swiping users session status.
-    @user_id = session[:current_user_id]
-    @restaurant.session.user_sessions.find_by(user_id: @user_id).update(status: 1)
-
-
-    # checks if the session is complete.
-    @swipe_count = 0
-    @restaurant.session.restaurants.each do |restaurant|
-      @swipe_count += restaurant.swipes.length
+    if(swipe_count >= @session.users.length * 2)
+      true
+    else
+      false
     end
+  end
 
-    puts 'user count:'
-    puts @restaurant.session.users.length
-    if(@swipe_count >= @restaurant.session.users.length * 2)
-      puts 'SESSION COMPLETE'
-      @restaurant.session.user_sessions.each do |user_session|
-        user_session.status = 2
-        puts user_session.update(status: 2)
-      end
+  # Updates the status for all invited users to Finished.
+  def set_session_to_finished
+    @session.user_sessions.each do |user_session|
+      user_session.update(status: 2)
     end
   end
 end
