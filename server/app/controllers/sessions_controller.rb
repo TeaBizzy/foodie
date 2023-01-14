82d1c71e1869ec073933609309f49db3f01965a1
Stefan Talbot <satalbot@protonmail.com>
2023-01-14 01:18:03 -0500

    Implement Session Deletion
    + Added: Deletes specified user_session from the database.
    + Added: Deletes the entire session if all other users have left.

--- server/app/controllers/sessions_controller.rb
+++ server/app/controllers/sessions_controller.rb
@@ -63,4 +63,18 @@ class SessionsController < ApplicationController
 
     return true
   end
+
+  def destroy
+    user_id = session[:current_user_id]
+    session_id = params[:id]
+    session = Session.find_by(id: session_id)
+
+    if(session.users.length < 2)
+      session.destroy()
+    else
+      user_session = session.user_sessions.find_by(user_id: user_id)
+      user_session.destroy()
+      # TODO: Perhaps just have the whol session deleted?
+    end
+  end
 end
